33be5d6b8fe6ca79f4c7c41706c169a8d8ba58cd
Patrick Koller <kollepa5@students.zhaw.ch>
2024-03-20 20:38:08 +0100

    wip

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,8 @@ import App from "./App.tsx";
 
 import "./styles/index.css";
 
-const root = document.getElementById("root");
-if (root) {
-    ReactDOM.createRoot(root).render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
