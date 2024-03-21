cb664820ca54a58f89316436a53e9bc91c6bd956
Patrick Koller <kollepa5@students.zhaw.ch>
2024-03-21 15:51:04 +0100

    fixed unit test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { DevelopmentPage } from "./pages/DevelopmentPage";
