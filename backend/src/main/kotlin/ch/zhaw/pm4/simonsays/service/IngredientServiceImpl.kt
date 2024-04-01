f9d7ad10018c7a53fb393d54cae523c7a85bf0e7
Lara <waesplar@students.zhaw.ch>
2024-04-01 20:44:02 +0200

    added 404 error

--- backend/src/main/kotlin/ch/zhaw/pm4/simonsays/service/IngredientServiceImpl.kt
+++ backend/src/main/kotlin/ch/zhaw/pm4/simonsays/service/IngredientServiceImpl.kt
@@ -3,6 +3,7 @@ package ch.zhaw.pm4.simonsays.service
 import ch.zhaw.pm4.simonsays.api.mapper.IngredientMapper
 import ch.zhaw.pm4.simonsays.api.types.IngredientCreateDTO
 import ch.zhaw.pm4.simonsays.api.types.IngredientDTO
+import ch.zhaw.pm4.simonsays.exception.ResourceNotFoundException
 import ch.zhaw.pm4.simonsays.repository.IngredientRepository
 import org.springframework.stereotype.Service
 
@@ -20,13 +21,15 @@ class IngredientServiceImpl (
     }
 
     override fun getIngredient(id: Long): IngredientDTO {
-        return ingredientMapper.mapToIngredientDTO(ingredientRepository.findById(id).get())
+        return ingredientMapper.mapToIngredientDTO(ingredientRepository.findById(id).orElseThrow {
+            ResourceNotFoundException("Ingredient not found with ID: $id")
+        })
     }
 
     override fun deleteIngredient(id: Long) {
-//        ingredientRepository.findById(id).orElseThrow {
-//            ResourceNotFoundException("Event not found with ID: $id")
-//        }
+        ingredientRepository.findById(id).orElseThrow {
+            ResourceNotFoundException("Ingredient not found with ID: $id")
+        }
         ingredientRepository.deleteById(id)
     }
 
