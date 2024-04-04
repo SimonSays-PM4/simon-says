624065b0d340b86b68fd83aec2238e7c51ba5a76
Dragana Ristic <ristcdragana@gmail.ch>
2024-04-04 09:10:07 +0200

    testing on localhost

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -1,6 +1,6 @@
 describe("Login", () => {
     it("Should log in without issues", () => {
-        cy.visit("https://simonsays-stage.pm4.init-lab.ch")
+        cy.visit("http://localhost:3000")
         cy.get('h1').contains('Login')
         cy.get('form').contains('Login').click()
         cy.get('h1').contains('Home Page')
