4e42a1a7f3dcfee52940af95c043d313058aea78
Leo Ferrari <ferrale1@students.zhaw.ch>
2024-04-07 15:03:09 +0200

    KAN-103: Added debugging

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,6 +2,8 @@ import {EventControllerApi} from "./gen/api";
 
 const API_URL = process.env.VITE_API_URL;
 
+console.log("API ROUTE: "+API_URL)
+console.log("STAGE: "+import.meta.env.MODE)
 const eventService = new EventControllerApi(undefined,API_URL, undefined);
 
 export{eventService}
