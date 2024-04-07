4ecaa54e3a692c1a29ad2abf036e6c4906817b38
Leo Ferrari <ferrale1@students.zhaw.ch>
2024-04-07 17:43:24 +0200

    KAN-103: Added status bar

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,9 +1,10 @@
-import {EventControllerApi} from "./gen/api";
+import {EventControllerApi, HealthControllerApi} from "./gen/api";
 
-const API_URL = process.env.VITE_API_URL;
+export const API_URL = process.env.VITE_API_URL || import.meta.env.VITE_API_URL;
 
 console.log("API ROUTE: "+API_URL)
 console.log("STAGE: "+import.meta.env.MODE)
-const eventService = new EventControllerApi(undefined,API_URL || import.meta.env.VITE_API_URL, undefined);
+const eventService = new EventControllerApi(undefined,API_URL, undefined);
+const healthService = new HealthControllerApi(undefined,API_URL || import.meta.env.VITE_API_URL, undefined);
 
-export{eventService}
+export{eventService,healthService}
