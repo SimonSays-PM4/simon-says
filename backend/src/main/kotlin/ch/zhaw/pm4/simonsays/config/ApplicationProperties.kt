7b6f525e796d28b83ff2b231e2eb8d6faa789ec2
Micha Guillet <guillmic@students.zhaw.ch>
2024-04-11 22:05:52 +0200

    fixed tests

--- backend/src/main/kotlin/ch/zhaw/pm4/simonsays/config/ApplicationProperties.kt
+++ backend/src/main/kotlin/ch/zhaw/pm4/simonsays/config/ApplicationProperties.kt
@@ -1,9 +1,11 @@
 package ch.zhaw.pm4.simonsays.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
 
+@Component
 @ConfigurationProperties(prefix = "application")
-class ApplicationProperties (
-    val url: String,
-    val adminToken : String
-)
+class ApplicationProperties {
+    var url: String = ""
+    var adminToken: String = ""
+}
