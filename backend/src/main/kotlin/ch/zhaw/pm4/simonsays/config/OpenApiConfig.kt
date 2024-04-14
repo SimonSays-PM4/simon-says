b152ad290f621409e6b05ee546347052641cedb9
Micha Guillet <guillmic@students.zhaw.ch>
2024-04-14 08:18:29 +0200

    fixed swagger authentication

--- backend/src/main/kotlin/ch/zhaw/pm4/simonsays/config/OpenApiConfig.kt
+++ backend/src/main/kotlin/ch/zhaw/pm4/simonsays/config/OpenApiConfig.kt
@@ -1,18 +1,26 @@
 package ch.zhaw.pm4.simonsays.config
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfig{
+@SecurityScheme(
+    name = "basicAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "basic"
+)
+class OpenApiConfig {
     @Bean
     fun openApi(): OpenAPI = OpenAPI()
-            .info(
-                    Info()
-                            .title("SimonSays")
-                            .version("1.0.0")
+        .info(
+            Info()
+                .title("SimonSays")
+                .version("1.0.0")
 
-            )
+        )
 }
