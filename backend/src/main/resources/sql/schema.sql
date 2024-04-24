4f2a03dd3e03f7a20e4c85a84feb9d871740e071
Retype GitHub Action <retype@object.net>
2024-04-24 08:16:23 +0000

    Refreshes Retype-generated documentation.
    Process triggered by michaguillet.

---
+++ backend/src/main/resources/sql/schema.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS `simonsays` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `simonsays`;
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+
+-- Schema for Users table
+/*CREATE TABLE IF NOT EXISTS event
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL,
+    numberOfTables    INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS menu_item
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    event_id INT NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES event (id)
+    );
+
+CREATE TABLE IF NOT EXISTS ingredient
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    event_id INT NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES event (id)
+);*/
+
+/*CREATE TABLE IF NOT EXISTS menu_item_ingredients
+(
+    menu_item_id INT NOT NULL,
+    FOREIGN KEY (menu_item_id) REFERENCES menu_item (id),
+    ingredient_id INT NOT NULL,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient (id)
+);*/
+
