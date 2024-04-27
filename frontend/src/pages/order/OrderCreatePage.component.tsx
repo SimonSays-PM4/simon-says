1ee6c2e6e8cd8fc103bf7a32c24522b2f5bd5a74
Patrick Koller <kollerpatrick@outlook.com>
2024-04-27 18:27:55 +0200

    added dialog and improved order edit

--- frontend/src/pages/order/OrderCreatePage.component.tsx
+++ frontend/src/pages/order/OrderCreatePage.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useOrderCreatePage } from "./OrderCreatePage.hooks.tsx";
 import { Loader } from "../../components/Loader.tsx";
 import { useForm } from "react-hook-form";
@@ -14,17 +14,25 @@ import { Tab } from "@headlessui/react";
 import { OrderMenuModel } from "../../models/OrderMenuModel.ts";
 import { OrderMenuItemModel } from "../../models/OrderMenuItemModel.ts";
 import { OrderIngredientModel } from "../../models/OrderIngredientModel.ts";
+import { Dialog } from "../../components/Dialog.tsx";
 
 export const OrderCreatePageComponent: React.FC = () => {
     const { isLoading, isSaving, menuList, selectedMenus, setSelectedMenus, menuItemList, selectedMenuItems, setSelectedMenuItems, orderActions, isTakeAway, setIsTakeAway } = useOrderCreatePage();
 
-    const menuIndex = React.useRef<number>(0);
-    const menuItemIndex = React.useRef<number>(0);
+    const menuIndex = useRef<number>(0);
+    const menuItemIndex = useRef<number>(0);
+
+    const [isEditMenuItemDialogOpen, setIsEditMenuItemDialogOpen] = useState<boolean>(false);
+    const [menuItemToEdit, setMenuItemToEdit] = useState<OrderMenuItemModel | undefined>(undefined);
+
+    const [isEditMenuDialogOpen, setIsEditMenuDialogOpen] = useState<boolean>(false);
+    const [menuToEdit, setMenuToEdit] = useState<OrderMenuModel | undefined>(undefined);
 
     const {
         handleSubmit,
         register,
-        getValues
+        getValues,
+        setValue
     } = useForm();
     const navigate = useNavigate();
 
@@ -38,6 +46,33 @@ export const OrderCreatePageComponent: React.FC = () => {
         menuItemIndex.current = menuItemIndex.current + 1;
     };
 
+    const removeMenu = (menuIndex: number) => {
+        setSelectedMenus(selectedMenus.filter((selectedMenu) => selectedMenu.index !== menuIndex));
+    }
+
+    const removeMenuItem = (menuItemIndex: number) => {
+        setSelectedMenuItems(selectedMenuItems.filter((selectedMenuItem) => selectedMenuItem.index !== menuItemIndex));
+    }
+
+    const removeMenuItemFromMenu = (menuIndex: number, menuItem: OrderMenuItemModel) => {
+        const selectedMenu = selectedMenus.find((selectedMenu) => selectedMenu.index === menuIndex);
+        if (selectedMenu && selectedMenu.menuItems.length > 1) { // can not delete last menu item
+            selectedMenu.menuItems = selectedMenu.menuItems.filter((selectedMenuItem) => selectedMenuItem.id !== menuItem.id);
+            setSelectedMenus([...selectedMenus]);
+        }
+    }
+
+    const removeIngredientFromMenuItemInMenu = (menuIndex: number, menuItemIndex: number, ingredient: IngredientDTO) => {
+        const selectedMenu = selectedMenus.find((selectedMenu) => selectedMenu.index === menuIndex);
+        if (selectedMenu) {
+            const selectedMenuItem = selectedMenu.menuItems.find((selectedMenuItem) => selectedMenuItem.index === menuItemIndex);
+            if (selectedMenuItem && selectedMenuItem.ingredients.length > 1) { // can not delete last ingredients
+                selectedMenuItem.ingredients = selectedMenuItem.ingredients.filter((selectedIngredient) => selectedIngredient.id !== ingredient.id);
+                setSelectedMenus([...selectedMenus]);
+            }
+        }
+    };
+
     const removeIngredientFromMenuItem = (menuItemIndex: number, ingredient: IngredientDTO) => {
         const selectedMenuItem = selectedMenuItems.find((selectedMenu) => selectedMenu.index === menuItemIndex);
         if (selectedMenuItem && selectedMenuItem.ingredients.length > 1) { // can not delete last ingredients
@@ -45,6 +80,31 @@ export const OrderCreatePageComponent: React.FC = () => {
             setSelectedMenuItems([...selectedMenuItems]);
         }
     };
+
+    const updateMenuItem = (updatedMenuItem?: OrderMenuItemModel) => {
+        if (updatedMenuItem) {
+            const index = selectedMenuItems.findIndex((selectedMenu) => selectedMenu.index === updatedMenuItem.index);
+            if (index !== -1) {
+                selectedMenuItems[index] = updatedMenuItem;
+                setSelectedMenuItems([...selectedMenuItems]);
+            }
+        }
+    }
+
+    const updateMenu = (updatedMenu?: OrderMenuModel) => {
+        if (updatedMenu) {
+            const index = selectedMenus.findIndex((selectedMenu) => selectedMenu.index === updatedMenu.index);
+            if (index !== -1) {
+                selectedMenus[index] = updatedMenu;
+                setSelectedMenus([...selectedMenus]);
+            }
+        }
+    }
+
+    const onIsTakeAwayChanged = (isTakeAway: boolean) => {
+        setValue(nameof<OrderCreateDTO>(e => e.tableNumber), undefined);
+        setIsTakeAway(isTakeAway);
+    }
 
     return (
         <div className="w-full">
@@ -65,7 +125,7 @@ export const OrderCreatePageComponent: React.FC = () => {
                                         <input
                                             id="isTakeAway"
                                             className="form-input rounded-lg border border-default-200 px-4 py-2.5 scale-[2] ml-2 mt-3 mb-6"
-                                            onChange={(e) => setIsTakeAway(e.target.checked)}
+                                            onChange={(e) => onIsTakeAwayChanged(e.target.checked)}
                                             type="checkbox"
                                         />
                                     </div>
@@ -154,54 +214,48 @@ export const OrderCreatePageComponent: React.FC = () => {
                             </Tab.Panels>
                         </Tab.Group>
 
-                        <div className="grid w-full border-t-2 mb-6">
+                        <div className="grid w-full border-2 rounded-lg p-2 mb-6">
                             <p>Selektierte Menus</p>
                             {selectedMenus.map((menu) => {
                                 return (
-                                    <div key={menu.index + menu.id} className="w-full">
-                                        <p className="font-bold">{menu.name}: {menu.price.toFixed(2)} CHF</p>
+                                    <div key={menu.index + menu.id} className="w-full flex my-2 border-b-2">
+                                        <div>
+                                            <p className="font-bold">{menu.name}: {menu.price.toFixed(2)} CHF</p>
 
-                                        {menu.menuItems.length > 0 ? (
-                                            <div className="grid">
-                                                {menu.menuItems.map((menuItem: OrderMenuItemModel) => (
-                                                    <div key={menu.index + menuItem.id}>
-                                                        <span>{menuItem.name}</span>
-                                                    </div>
-                                                ))}
-                                            </div>)
-                                            : (<></>)
-                                        }
+                                            {menu.menuItems.length > 0 ? (
+                                                <div className="grid">
+                                                    {menu.menuItems.map((menuItem: OrderMenuItemModel) => (
+                                                        <div key={menu.index + menuItem.id}>
+                                                            <span>{menuItem.name} - ({menuItem.ingredients.map((i) => i.name).join(", ")})</span>
+                                                        </div>
+                                                    ))}
+                                                </div>)
+                                                : (<></>)
+                                            }
+                                        </div>
+                                        <div className="ml-4">
+                                            <Button buttonText="Bearbeiten" className="my-2" buttonType={ButtonType.Secondary} onClick={() => { setMenuToEdit(menu); setIsEditMenuDialogOpen(true); }} />
+                                            <Button buttonText="Entfernen" className="my-2" buttonType={ButtonType.Secondary} onClick={() => removeMenu(menu.index)} />
+                                        </div>
                                     </div>
                                 );
                             })}
                         </div>
 
-                        <div className="grid w-full border-t-2 mb-12">
+                        <div className="grid w-full border-2 rounded-lg p-2 mb-12">
                             <p>Selektierte Menu Items</p>
                             {selectedMenuItems.map((menuItem) => {
                                 return (
-                                    <div key={menuItem.id + menuItem.index} className="w-full">
-                                        <p className="font-bold">{menuItem.name}: {menuItem.price.toFixed(2)} CHF</p>
+                                    <div key={menuItem.id + menuItem.index} className="w-full flex my-2 border-b-2">
+                                        <div>
+                                            <p className="font-bold">{menuItem.name}: {menuItem.price.toFixed(2)} CHF</p>
+                                            <span>{menuItem.name} - ({menuItem.ingredients.map((i) => i.name).join(", ")})</span>
+                                        </div>
 
-                                        {menuItem.ingredients.length > 0 ? (
-                                            <div className="grid">
-                                                {menuItem.ingredients.map((ingredient: OrderIngredientModel) => (
-                                                    <div key={menuItem.index + ingredient.id}>
-                                                        <span>{ingredient.name}</span>
-
-                                                        {menuItem.ingredients.length > 1
-                                                            ? (
-                                                                <a className="text-primary cursor-pointer ml-4" onClick={() => removeIngredientFromMenuItem(menuItem.index, ingredient)}>
-                                                                    {"<- Entfernen"}
-                                                                </a>
-                                                            )
-                                                            : (<></>)
-                                                        }
-                                                    </div>
-                                                ))}
-                                            </div>)
-                                            : (<></>)
-                                        }
+                                        <div className="ml-4">
+                                            <Button buttonText="Bearbeiten" className="my-2" buttonType={ButtonType.Secondary} onClick={() => { setMenuItemToEdit(menuItem); setIsEditMenuItemDialogOpen(true); }} />
+                                            <Button buttonText="Entfernen" className="my-2" buttonType={ButtonType.Secondary} onClick={() => removeMenuItem(menuItem.index)} />
+                                        </div>
                                     </div>
                                 );
                             })}
@@ -218,6 +272,79 @@ export const OrderCreatePageComponent: React.FC = () => {
                     </form>
                 </>)
             }
-        </div>
+
+            <Dialog title={`Menu "${menuToEdit?.name}" bearbeiten`}
+                isOpen={isEditMenuDialogOpen}
+                cancelAction={() => setIsEditMenuDialogOpen(false)}
+                cancelText="Abbrechen"
+                okAction={() => { updateMenu(menuToEdit); setIsEditMenuDialogOpen(false); }}
+                okText="Speichern"
+                setIsOpen={setIsEditMenuDialogOpen}>
+                <>
+                    {menuToEdit && menuToEdit.menuItems.length > 0 ? (
+                        <div className="grid">
+                            {menuToEdit.menuItems.map((menuItem: OrderMenuItemModel) => (
+                                <div key={menuToEdit.index + menuItem.id} className="rounded-lg border-2 mb-4">
+                                    <div className="grid grid-cols-2 border-b-2 pb-2 mb-2 pl-3">
+                                        <p className="my-auto">{menuItem.name}</p>
+
+                                        {menuToEdit.menuItems.length > 1
+                                            ? (
+                                                <Button buttonText="Entfernen" className="my-2" buttonType={ButtonType.Secondary} onClick={() => removeMenuItemFromMenu(menuToEdit.index, menuItem)} />
+                                            )
+                                            : (<></>)
+                                        }
+                                    </div>
+
+                                    <div className="grid ml-6">
+                                        {menuItem.ingredients.map((ingredient: OrderIngredientModel) => (
+                                            <div key={menuItem.index + ingredient.id} className="grid grid-cols-2 border-b-2">
+                                                <p className="my-auto">{ingredient.name}</p>
+
+                                                {menuItem.ingredients.length > 1
+                                                    ? (
+                                                        <Button buttonText="Entfernen" className="my-2" buttonType={ButtonType.Secondary} onClick={() => removeIngredientFromMenuItemInMenu(menuToEdit.index, menuItem.index, ingredient)} />
+                                                    )
+                                                    : (<></>)
+                                                }
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>)
+                        : (<p></p>)
+                    }
+                </>
+            </Dialog>
+
+            <Dialog title={`Menu Item "${menuItemToEdit?.name}" bearbeiten`}
+                isOpen={isEditMenuItemDialogOpen}
+                cancelAction={() => setIsEditMenuItemDialogOpen(false)}
+                cancelText="Abbrechen"
+                okAction={() => { updateMenuItem(menuItemToEdit); setIsEditMenuItemDialogOpen(false); }}
+                okText="Speichern"
+                setIsOpen={setIsEditMenuItemDialogOpen}>
+                <>
+                    {menuItemToEdit && menuItemToEdit.ingredients.length > 0 ? (
+                        <div className="grid">
+                            {menuItemToEdit.ingredients.map((ingredient: OrderIngredientModel) => (
+                                <div key={menuItemToEdit.index + ingredient.id} className="grid grid-cols-2 border-b-2">
+                                    <p className="my-auto">{ingredient.name}</p>
+
+                                    {menuItemToEdit.ingredients.length > 1
+                                        ? (
+                                            <Button buttonText="Entfernen" className="my-2" buttonType={ButtonType.Secondary} onClick={() => removeIngredientFromMenuItem(menuItemToEdit.index, ingredient)} />
+                                        )
+                                        : (<></>)
+                                    }
+                                </div>
+                            ))}
+                        </div>)
+                        : (<p></p>)
+                    }
+                </>
+            </Dialog>
+        </div >
     );
 }
