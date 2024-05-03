77425e5a305d1f5c1f09566173527ca562388a52
Dragana Ristic <ristcdragana@gmail.ch>
2024-05-03 17:50:46 +0200

    KAN-141: fix unused params

--- frontend/src/pages/user/StationSelectionPage.component.tsx
+++ frontend/src/pages/user/StationSelectionPage.component.tsx
@@ -1,8 +1,7 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Card } from "../../components/Card.tsx";
 import { EventContext } from "../../providers/EventContext.tsx";
-import { AppContext } from "../../providers/AppContext.tsx";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { PiReceiptDuotone, PiChargingStationDuotone } from "react-icons/pi";
 import { useStationSelectionPage } from "./StationSelectionPage.hooks.ts";
 
@@ -10,7 +9,6 @@ import { useStationSelectionPage } from "./StationSelectionPage.hooks.ts";
 export const StationSelectionPage: React.FC = () => {
     const { eventId } = useContext(EventContext);
     const navigate = useNavigate();
-    const appContext = useContext(AppContext);
     const {stationList, isLoading} = useStationSelectionPage();
     
     return (
