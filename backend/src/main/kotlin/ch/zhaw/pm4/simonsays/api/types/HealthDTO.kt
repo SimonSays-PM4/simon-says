0a574820d5c1823e864bc4baeada83fdcb7e3558
michaguillet <100429292+michaguillet@users.noreply.github.com>
2024-05-03 08:04:38 +0200

    feat: add health endpoint KAN-143 (#75)
    * add health endpoint
    
    * cleanup

---
+++ backend/src/main/kotlin/ch/zhaw/pm4/simonsays/api/types/HealthDTO.kt
@@ -0,0 +1,10 @@
+package ch.zhaw.pm4.simonsays.api.types
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class HealthDTO(
+    @field:Schema(description = "Shows the state of the application.")
+    val state: String,
+    @field:Schema(description = "Shows the version of the application.")
+    val version: String
+)
