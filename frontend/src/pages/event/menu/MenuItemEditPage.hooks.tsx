41ddddaefeb1e6502a186b4b7fab286649703dc9
Patrick Koller <kollerpatrick@outlook.com>
2024-05-06 19:51:43 +0200

    fixed menu item edit

--- frontend/src/pages/event/menu/MenuItemEditPage.hooks.tsx
+++ frontend/src/pages/event/menu/MenuItemEditPage.hooks.tsx
@@ -1,12 +1,13 @@
 import { useCallback, useContext, useEffect, useState } from "react";
 import { getIngredientService, getMenuItemService } from "../../../api.ts";
 import { useNavigate, useParams } from "react-router-dom";
-import {IngredientDTO, MenuItemCreateUpdateDTO} from "../../../gen/api";
-import { FieldValues } from "react-hook-form";
+import { IngredientDTO, MenuItemCreateUpdateDTO } from "../../../gen/api";
+import { Control, FieldErrors, FieldValues, UseFormGetValues, UseFormHandleSubmit, UseFormRegister, useForm } from "react-hook-form";
 import { EventContext } from "../../../providers/EventContext.tsx";
 import { AppContext } from "../../../providers/AppContext.tsx";
 import { NotificationType } from "../../../enums/NotificationType.ts";
 import { ValueLabel } from "../../../models/ValueLabel.ts";
+import { nameof } from "ts-simple-nameof";
 
 type MenuItemEditReturnProps = {
     menuItem: MenuItemCreateUpdateDTO,
@@ -18,11 +19,17 @@ type MenuItemEditReturnProps = {
     setIngredients: (ingredients: IngredientDTO[]) => void
     ingredientOptions: ValueLabel[],
     selectedIngredients: ValueLabel[]
+
+    formRegister: UseFormRegister<FieldValues>;
+    handleSubmit: UseFormHandleSubmit<FieldValues, undefined>;
+    formErrors: FieldErrors<FieldValues>;
+    formGetValues: UseFormGetValues<FieldValues>;
+    formControl: Control<FieldValues, any>;
 };
 
 type MenuItemActions = {
     saveMenuItem: (menuItemToSave: FieldValues) => void,
-    onFormInvalid: (data:FieldValues) => void,
+    onFormInvalid: (data: FieldValues) => void,
     deleteMenuItem: () => void,
 }
 
@@ -35,7 +42,7 @@ export const useMenuItemEditPage = (): MenuItemEditReturnProps => {
     const [menuItem, setMenuItem] = useState<MenuItemCreateUpdateDTO>({ id: 0, name: "", price: 0.0, ingredients: [] });
 
     const [selectedIngredients, setSelectedIngredients] = useState<ValueLabel[]>([]);
-    const [isLoading, setLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [options, setOptions] = useState<ValueLabel[]>([]);
     const navigate = useNavigate();
@@ -43,22 +50,42 @@ export const useMenuItemEditPage = (): MenuItemEditReturnProps => {
     const ingredientService = getIngredientService(appContext.loginInfo.userName, appContext.loginInfo.password);
     const menuItemService = getMenuItemService(appContext.loginInfo.userName, appContext.loginInfo.password);
 
-    useEffect(() => {
-        console.log(menuItemId)
-        if (eventId > 0) {
-            if (menuItemId > 0) {
-                menuItemService.getMenuItem(eventId, menuItemId).then((item) => {
-                    setSelectedIngredients(item.data.ingredients.map((ing) => {
-                        return { value: ing.id, label: ing.name }
-                    }))
-                    const menuItemCreateUpdateDTO : MenuItemCreateUpdateDTO  = {id:item.data.id, name:item.data.name,price:item.data.price,ingredients:item.data.ingredients};
-                    setMenuItem(menuItemCreateUpdateDTO)
-                }).catch(() => appContext.addNotification(NotificationType.ERR, "Failed to Fetch Menu Item"))
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        getValues,
+        setValue,
+        control
+    } = useForm();
+
+    const loadMenuItem = useCallback(async () => {
+        try {
+            const items = await ingredientService.listIngredients(eventId);
+            const availableIngredients = items.data;
+            setIngredients(availableIngredients);
+            setOptions([...availableIngredients.map((item) => { return { value: item.id, label: item.name } })]);
+
+            register(nameof<MenuItemCreateUpdateDTO>(e => e.ingredients), { value: [] });
+
+            if (menuItemId && menuItemId > 0) {
+                const response = await menuItemService.getMenuItem(eventId, menuItemId);
+                const receivedMenuItem = response.data as MenuItemCreateUpdateDTO;
+                setMenuItem(receivedMenuItem);
+
+                const receivedIngredients = receivedMenuItem.ingredients.map((item: IngredientDTO) => { return { value: item.id, label: item.name } });
+                setValue(nameof<MenuItemCreateUpdateDTO>(e => e.ingredients), Array.from(receivedIngredients));
             }
-            ingredientService.listIngredients(eventId).then((ingredients) => {
-                setOptions([...ingredients.data.map((ing) => { return { value: ing.id, label: ing.name } })]);
-            }).catch(() => appContext.addNotification(NotificationType.ERR, "Failed to Fetch Ingredients"))
         }
+        catch (_) {
+            appContext.addNotification(NotificationType.ERR, "Beim Laden des Menus ist ein Fehler aufgetreten.");
+        }
+    }, []);
+
+    useEffect(() => {
+        setIsLoading(() => true);
+        loadMenuItem()
+            .then(() => setIsLoading(() => false));
     }, [eventId, menuItemId])
 
     const onFormInvalid = (data?: FieldValues) => {
@@ -73,36 +100,36 @@ export const useMenuItemEditPage = (): MenuItemEditReturnProps => {
     const setIngredients = useCallback((ingredients: IngredientDTO[]) => {
         const newMenuItem = menuItem;
         newMenuItem.ingredients = ingredients;
-        setSelectedIngredients(ingredients.map((ing) => { return { value: ing.id, label: ing.name } }))
-        setMenuItem(newMenuItem)
+        setSelectedIngredients(ingredients.map((ing) => { return { value: ing.id, label: ing.name } }));
+        setMenuItem(newMenuItem);
     }, [menuItem, menuItemId])
 
     const saveMenuItem = (data: FieldValues) => {
-            setLoading(true);
+        setIsLoading(true);
 
-            const menuItemToSave = data as MenuItemCreateUpdateDTO;
-            menuItemToSave.id = menuItemId > 0 ? menuItemId : undefined;
-            menuItemToSave.ingredients = data["ingredients"].map((ing:ValueLabel) => { return { id: ing.value, name: ing.label } });
-            menuItemToSave.price = Number(data["price"])
+        const menuItemToSave = data as MenuItemCreateUpdateDTO;
+        menuItemToSave.id = menuItemId > 0 ? menuItemId : undefined;
+        menuItemToSave.ingredients = data["ingredients"].map((ing: ValueLabel) => { return { id: ing.value, name: ing.label } });
+        menuItemToSave.price = Number(data["price"]);
 
-            menuItemService
-                .putMenuItem(eventId, menuItemToSave)
-                .then(() => {
-                    setLoading(false);
-                    navigate("../menuItem");
-                    appContext.addNotification(NotificationType.OK, "Saved Menu Item \"" + menuItemToSave.name + "\"")
-                })
-                .catch(() => {
-                    setLoading(false);
-                    appContext.addNotification(NotificationType.ERR, "Failed to save \"" + menuItemToSave.name + "\"");
-                });
-        };
+        menuItemService
+            .putMenuItem(eventId, menuItemToSave)
+            .then(() => {
+                setIsLoading(false);
+                navigate("../menuItem");
+                appContext.addNotification(NotificationType.OK, "Saved Menu Item \"" + menuItemToSave.name + "\"")
+            })
+            .catch(() => {
+                setIsLoading(false);
+                appContext.addNotification(NotificationType.ERR, "Failed to save \"" + menuItemToSave.name + "\"");
+            });
+    };
 
     const deleteMenuItem = useCallback(() => {
         menuItemService.deleteMenuItem(eventId, menuItemId).then(response => {
             if (response.status == 200 || response.status == 201) {
-                navigate("./../")
-                appContext.addNotification(NotificationType.OK, "Deleted Menu Item")
+                navigate("./../");
+                appContext.addNotification(NotificationType.OK, "Deleted Menu Item");
             }
         })
 
@@ -114,5 +141,5 @@ export const useMenuItemEditPage = (): MenuItemEditReturnProps => {
         onFormInvalid: onFormInvalid
     };
 
-    return { menuItem, menuItemActions, isLoading, setShowDeleteModal, showDeleteModal, navigate, setIngredients, ingredientOptions: options, selectedIngredients }
+    return { menuItem, menuItemActions, isLoading, setShowDeleteModal, showDeleteModal, navigate, setIngredients, ingredientOptions: options, selectedIngredients, formErrors: errors, formGetValues: getValues, formRegister: register, formControl: control, handleSubmit }
 }
