534cbc3314b0c60d9a4447333151f048e67db056
Moritz Weirauch <moritz.weirauch@lumexus.ch>
2024-05-06 19:44:53 +0200

    Add mustBeProduced Flag to Ingredient KAN-127

--- backend/src/test/kotlin/ch/zhaw/pm4/simonsays/IngredientsTestData.kt
+++ backend/src/test/kotlin/ch/zhaw/pm4/simonsays/IngredientsTestData.kt
@@ -8,12 +8,13 @@ import ch.zhaw.pm4.simonsays.entity.Ingredient
 
 fun createUpdateTestIngredientDTO(id: Long? = null, name: String? = "TestIngredient") = IngredientCreateUpdateDTO(
     id = id,
-    name = name
+    name = name,
+    mustBeProduced = true
 )
 
-fun getTestIngredient1(name: String? = "TestIngredient") = Ingredient(name = name!!, id = 1, event = getTestEvent(), menuItems = null, stations = null)
+fun getTestIngredient1(name: String? = "TestIngredient") = Ingredient(name = name!!, id = 1, event = getTestEvent(), mustBeProduced = true, menuItems = null, stations = null)
 
-fun getTestIngredientDTO(name: String? = "TestIngredient", id: Long? = 1) = IngredientDTO(name = name!!, id = id!!)
+fun getTestIngredientDTO(name: String? = "TestIngredient", id: Long? = 1) = IngredientDTO(name = name!!, id = id!!, mustBeProduced = true)
 
 fun getTestEvent() = Event(id = 1, name = "TestEvent", password = "TestPassword", numberOfTables = 1)
 fun getTestEventDTO() = EventDTO(id = 1, name = "TestEvent", password = "TestPassword", numberOfTables = 1)
