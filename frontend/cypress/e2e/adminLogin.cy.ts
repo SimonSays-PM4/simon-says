4adbf9081744d47c442f61402ecd43ea79969a65
Patrick Koller <kollerpatrick@outlook.com>
2024-05-06 17:41:54 +0200

    fixes after merge

--- frontend/cypress/e2e/adminLogin.cy.ts
+++ frontend/cypress/e2e/adminLogin.cy.ts
@@ -1,6 +1,6 @@
 describe("Admin Login", () => {
     it("Should log in without issues", () => {
-        cy.visit("http://localhost:3000");
+        cy.visit("http://localhost:3000/login");
         cy.get("h1").contains("Login");
         cy.get("#code").type("mysecretpassword");
         cy.get("form").contains("Login").click();
--- frontend/src/routing/AuthorizedRoute.tsx
+++ frontend/src/routing/AuthorizedRoute.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AppContext } from "../providers/AppContext";
 import { ActivePageType } from "../enums/ActivePageType";
 import { Navigate } from "react-router-dom";
@@ -15,32 +15,30 @@ interface IAuthorizedRouteProps {
 export const AuthorizedRoute: React.FC<IAuthorizedRouteProps> = ({ children, activePageType }) => {
     const { loginInfo, setLoginInfo } = React.useContext(AppContext);
 
-    if (LoginInfo.isAuthenticated(loginInfo) === false) {
-        const encryptedPw = localStorage.getItem("encryptedCode");
-        console.log(encryptedPw);
-        if (!encryptedPw) {
-            <Navigate to={"/login?returnUrl=" + encodeURIComponent(document.location.pathname ?? "")} />
-        }
-        else {
-            const decryptedCode = decryptData(encryptedPw);
-            console.log(decryptedCode);
-            if (decryptedCode.split(":").length !== 2) {
+    useEffect(() => {
+        if (LoginInfo.isAuthenticated(loginInfo) === false) {
+            const encryptedPw = localStorage.getItem("encryptedCode");
+            if (!encryptedPw || encryptedPw === "") {
                 <Navigate to={"/login?returnUrl=" + encodeURIComponent(document.location.pathname ?? "")} />
             }
             else {
-                const splitCode = decryptedCode.split(":");
-                console.log(splitCode);
-
-                const eventService = getEventService(splitCode[0], splitCode[1]);
-                eventService.getEvents().then(() => {
-                    setLoginInfo(new LoginInfo(true, splitCode[0], splitCode[1]));
-                }).catch(() => {
-                    console.log("Invalid password");
-                });
-                setLoginInfo(() => new LoginInfo(true, splitCode[0], splitCode[1]));
+                const decryptedCode = decryptData(encryptedPw);
+                if (decryptedCode.split(":").length !== 2) {
+                    <Navigate to={"/login?returnUrl=" + encodeURIComponent(document.location.pathname ?? "")} />
+                }
+                else {
+                    const splitCode = decryptedCode.split(":");
+                    const eventService = getEventService(splitCode[0], splitCode[1]);
+                    eventService.getEvents().then(() => {
+                        setLoginInfo(new LoginInfo(true, splitCode[0], splitCode[1]));
+                    }).catch(() => {
+                        console.log("Invalid password");
+                    });
+                    setLoginInfo(() => new LoginInfo(true, splitCode[0], splitCode[1]));
+                }
             }
         }
-    }
+    }, [loginInfo, setLoginInfo]);
 
     return <MainLayout activePageType={activePageType}>{children}</MainLayout>
 };
