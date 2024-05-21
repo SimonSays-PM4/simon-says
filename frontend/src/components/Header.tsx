e56568f31f5beed4e5c9e0ccea5b5da3f8378799
Patrick Koller <kollerpatrick@outlook.com>
2024-05-21 20:43:33 +0200

    fixed join

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { AppContext } from "../providers/AppContext";
-import {NavLink, useNavigate} from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { LoginInfo } from "../models/LoginInfo";
 import { Button } from "./Button";
-import {MdFoodBank} from "react-icons/md";
-import {EventContext} from "../providers/EventContext.tsx";
+import { MdFoodBank } from "react-icons/md";
+import { EventContext } from "../providers/EventContext.tsx";
 
 export const Header: React.FC = () => {
     const { setLoginInfo, loginInfo } = React.useContext(AppContext);
@@ -12,9 +12,16 @@ export const Header: React.FC = () => {
     const navigate = useNavigate();
 
     const invokeLogout = () => {
+        const isAdmin = loginInfo.userName === "admin";
         localStorage.removeItem("encryptedCode");
         setLoginInfo(new LoginInfo(false, "-", ""));
-        navigate("/login");
+
+        if (isAdmin) {
+            navigate("/login");
+        }
+        else {
+            navigate(`/${eventId}/join`);
+        }
     };
 
     return (
@@ -22,20 +29,23 @@ export const Header: React.FC = () => {
             <div className="flex flex-wrap gap-3 justify-between">
                 <div className=" content-center text-2xl font-bold">
                     <div className="flex gap-4 items-center">
-                    <NavLink to={loginInfo.userName!="admin"?"/"+eventId:"/"} className="flex-shrink-0 flex items-center">
-                        <div className="border shadow rounded-md p-1 max-w-sm w-30 mx-auto">
-                            <div><MdFoodBank fontSize="40"/></div>
-                        </div>
-                    </NavLink>
-               <div>Simon Says</div></div></div><div className="p-4 flex gap-4">
-                <p className="ml-10 my-auto">{loginInfo.userName}</p>
-                <Button
-                    className="ml-5 my-auto"
-                    onClick={invokeLogout}
-                    type="button"
-                    buttonText="Logout"
-                />
-            </div>
+                        <NavLink to={loginInfo.userName != "admin" ? "/" + eventId : "/"} className="flex-shrink-0 flex items-center">
+                            <div className="border shadow rounded-md p-1 max-w-sm w-30 mx-auto">
+                                <div><MdFoodBank fontSize="40" /></div>
+                            </div>
+                        </NavLink>
+                        <div>Simon Says</div>
+                    </div>
+                </div>
+                <div className="p-4 flex gap-4">
+                    <p className="ml-10 my-auto">{loginInfo.userName}</p>
+                    <Button
+                        className="ml-5 my-auto"
+                        onClick={invokeLogout}
+                        type="button"
+                        buttonText="Logout"
+                    />
+                </div>
             </div>
         </div>
     );
